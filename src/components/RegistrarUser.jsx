1831d89ed5c93c84c8d6636150b221b9e81f9ef0
Santiago Padilla <padillasantiago51@gmail.com>
2025-02-08 12:33:56 -0300

    update styles createUser form

--- src/components/RegistrarUser.jsx
+++ src/components/RegistrarUser.jsx
@@ -60,34 +60,34 @@ const RegistrarUser = () => {
             <Navbar />
             <ToastContainer />
             <div className="bg-gray-100">
-                <button className="mt-4 ms-4 bg-slate-500 p-2 rounded hover:bg-slate-400" onClick={handleNavigate}>
+                <button className="mt-4 ms-4 mb-4 md:mb-0 lg:mb-0 bg-slate-500 p-2 rounded hover:bg-slate-400" onClick={handleNavigate}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="20 20" stroke-width="2" stroke={"#fff"} className="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                     </svg>
                 </button>
             </div>
             <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <form className="-mt-58 shadow-lg rounded-md mx-3 bg-white flex flex-col p-4 gap-3 lg:w-1/2 lg:mx-auto lg:-mt-36" onSubmit={(e) => handleSubmit(e)}>
-                    <h3 className="text-xl font-bold">Completa todos los campos</h3>
+                <form className="mt-10 mx-4 drop-shadow-lg bg-gray-100 flex flex-col p-6 gap-4 rounded-lg lg:w-1/3 lg:mx-auto border border-gray-200" onSubmit={handleSubmit}>
+                    <h3 className="text-xl font-bold bg-white p-3 rounded-md text-center uppercase">Completa todos los campos</h3>
 
                     <div className="flex flex-col">
-                        <label>Nombre:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="text" name="nombre" required onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">Nombre:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" name="nombre" required onChange={handleChange} />
                     </div>
 
                     <div className="flex flex-col">
-                        <label>Apellido:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="text" name="apellido" required onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">Apellido:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" name="apellido" required onChange={handleChange} />
                     </div>
 
                     <div className="flex flex-col">
-                        <label>DNI:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="text" minLength={8} maxLength={8} name="dni" required onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">DNI:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" minLength={8} maxLength={8} name="dni" required onChange={handleChange} />
                     </div>
 
-                    <div className="block items-center justify-between mt-3">
-                        <label>Especialidad:</label>
-                        <select className="mt-4" onChange={(e) => handleChange(e)} name="especialidad" required>
+                    <div className="flex flex-col">
+                        <label className="font-semibold">Especialidad:</label>
+                        <select className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" name="especialidad" required onChange={handleChange}>
                             <option value="">Seleccione una especialidad</option>
                             <option value="cardiología">Cardiología</option>
                             <option value="traumatologia">Traumatología</option>
@@ -101,26 +101,28 @@ const RegistrarUser = () => {
                     </div>
 
                     <div className="flex flex-col">
-                        <label>Correo electrónico:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="email" name="email" onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">Correo electrónico:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" name="email" required onChange={handleChange} />
                     </div>
 
                     <div className="flex flex-col">
-                        <label>Telefono:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="text" name="telefono" minLength={10} maxLength={10} required onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">Teléfono:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" name="telefono" minLength={10} maxLength={10} required onChange={handleChange} />
                     </div>
 
                     <div className="flex flex-col">
-                        <label type="text" name="password" required>Contraseña:</label>
-                        <input className="p-1 bg-slate-200 rounded-md" type="text" name="password" onChange={(e) => handleChange(e)} />
+                        <label className="font-semibold">Contraseña:</label>
+                        <input className="p-2 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" type="password" name="password" required onChange={handleChange} />
                     </div>
 
                     {cargando ? (
-                        <button className="rounded-md bg-blue-800 text-white p-2 hover:bg-blue-600 mt-5" type="submit">
-                            <ClipLoader color={'#fff'} />
+                        <button className="rounded-md bg-blue-800 text-white p-3 hover:bg-blue-600 mt-5 flex items-center justify-center" type="submit">
+                            <ClipLoader color={'#fff'} size={20} />
                         </button>
                     ) : (
-                        <button className="rounded-md bg-blue-800 text-white p-2 hover:bg-blue-600 mt-5" type="submit" onClick={e => handleSubmit(e)}>Crear Usuario</button>
+                        <button className="rounded-md bg-blue-800 text-white p-3 hover:bg-blue-600 mt-5 transition-all duration-200" type="submit">
+                            Crear Usuario
+                        </button>
                     )}
                 </form>
             </div>
